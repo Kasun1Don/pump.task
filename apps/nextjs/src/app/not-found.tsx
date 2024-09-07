a986b667f0cffd88ee49df8e383073c64560ff67
Kasun1Don <Kasunsldon@gmail.com>
2024-09-07 23:58:43 +1000

    fix: not-found page

--- apps/nextjs/src/app/not-found.tsx
+++ apps/nextjs/src/app/not-found.tsx
@@ -1,10 +1,10 @@
 export default function NotFound() {
-    return (
-        <div className="flex h-screen items-center justify-center">
-            <div className="text-center">
-                <h1 className="text-4xl font-bold mb-2">404 Page not found</h1>
-                <p className="text-xl">Couldn't find the requested resource</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="mb-2 text-4xl font-bold">404 Page not found</h1>
+        <p className="text-xl">Couldn't find the requested resource</p>
+      </div>
+    </div>
+  );
 }
