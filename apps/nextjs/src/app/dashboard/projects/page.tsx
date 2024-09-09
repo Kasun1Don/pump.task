5c8758001e8e176cc7a037ece7300e250ee9c5a1
Kasun1Don <Kasunsldon@gmail.com>
2024-09-09 23:59:59 +1000

    fix: project dashboard data

--- apps/nextjs/src/app/dashboard/projects/page.tsx
+++ apps/nextjs/src/app/dashboard/projects/page.tsx
@@ -1,11 +1,21 @@
 const projects = [
-  { id: 1, name: "CRM" }, // replace with correct paths
+  { id: 1, name: "CRM" }, // seed data
   { id: 2, name: "Dog Walkies" },
 ];
 
+const templates = [
+  { id: 1, name: "CRM" }, // seed data
+  { id: 2, name: "Dog Walkies" },
+];
+
+
+// TODO: Task Board Templates, Your Task Boards, start pumping out tasks faster for your next projectwith a pump.task board template:
 export default function ProjectsPage() {
   return (
     <div className="grid grid-cols-3 gap-4 p-8">
+      <div className="flex items-center justify-center rounded-lg bg-gray-800">
+
+      </div>
       {projects.map((project) => (
         <div
           key={project.id}
@@ -15,7 +25,7 @@ export default function ProjectsPage() {
         </div>
       ))}
       <div className="flex items-center justify-center rounded-lg bg-gray-800">
-        <h3 className="text-white">Create new board</h3>
+        <h3 className="text-white">Create new task board project</h3>
       </div>
     </div>
   );
--- apps/nextjs/src/app/not-found.tsx
+++ apps/nextjs/src/app/not-found.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
+
 import { Button } from "@acme/ui/button";
 
 export default function NotFound() {
   return (
-    <div className="bg-custom-bg flex h-screen items-center justify-center bg-cover bg-center"> 
+    <div className="bg-custom-bg flex h-screen items-center justify-center bg-cover bg-center">
       <div className="text-center">
         <h1 className="mb-2 text-4xl font-bold">404 Page not found</h1>
         <p className="text-xl">Couldn't find the requested resource</p>
-        
+
         <Link href="/">
-            {/* TODO: update button style to match the project design guide */}
-          <Button className="mt-4" variant="primary" size="md">Return to Home</Button> 
+          {/* TODO: update button style to match the project design guide */}
+          <Button className="mt-4" variant="primary" size="md">
+            Return to Home
+          </Button>
         </Link>
       </div>
     </div>
