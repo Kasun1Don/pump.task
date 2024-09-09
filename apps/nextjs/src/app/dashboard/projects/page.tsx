32fb02fc2ecdb72ef5a890e4d6c2b5a42f3e19e1
Kasun1Don <Kasunsldon@gmail.com>
2024-09-10 03:49:39 +1000

    feat(ui): add contrast, readability and theme styling

--- apps/nextjs/src/app/dashboard/projects/page.tsx
+++ apps/nextjs/src/app/dashboard/projects/page.tsx
@@ -1,6 +1,6 @@
 const projects = [
   { id: 1, name: "CRM" }, // seed data
-  { id: 2, name: "Dog Walkies" },
+  { id: 2, name: "Dog Walkies App" },
 ];
 
 const templates = [
@@ -9,44 +9,49 @@ const templates = [
   { id: 3, name: "Agile Sprint Board Template" },
 ];
 
-// TODO: Task Board Templates, Your Task Boards, 
+// TODO: card fills with related images + UI adjustments
 export default function ProjectsPage() {
   return (
     <>
-    <div className="m-8">
-      <div className="mb-8">
-        <div className="pl-8 pr-8">
-         <h1 className="text-xl font-bold">Your Project Task Boards</h1>
-        </div>
-        <div className="grid grid-cols-3 gap-4 p-8 auto-rows-min">
-          {projects.map((project) => (
-            <div
-              key={project.id}
-              className="overflow-hidden rounded-lg bg-gray-700 min-h-32 font-bold">
-              <h3 className="p-4 text-white">{project.name}</h3>
+      <div className="m-8">
+        <div className="mb-8">
+          <div className="pl-8 pr-8">
+            <h1 className="text-xl font-bold">Your Project Task Boards</h1>
+          </div>
+          <div className="grid auto-rows-min grid-cols-3 gap-4 p-8">
+            {projects.map((project) => (
+              <div
+                key={project.id}
+                className="min-h-32 overflow-hidden rounded-lg bg-[#09090B] font-bold border border-gray-700"
+              >
+                <h3 className="p-4 text-white">{project.name}</h3>
+              </div>
+            ))}
+            <div className="flex min-h-32 items-center justify-center rounded-lg bg-[#72D524] border border-gray-700">
+              <h3 className="text-[#18181B]">Create new task board</h3>
             </div>
-          ))}
-          <div className="flex items-center justify-center rounded-lg bg-gray-800 min-h-32">
-            <h3 className="text-white">Create new project task board</h3>
+          </div>
+        </div>
+        <div>
+          <div className="pl-8 pr-8">
+            <h1 className="mb-4 text-xl font-bold">Task Board Templates</h1>
+            <p className="font-bold">
+              Start your project with a Pump.task template to start pumping
+              through tasks faster:
+            </p>
+          </div>
+          <div className="grid auto-rows-min grid-cols-3 gap-4 p-8">
+            {templates.map((template) => (
+              <div
+                key={template.id}
+                className="min-h-32 overflow-hidden rounded-lg bg-[#18181B] font-bold border border-gray-700"
+              >
+                <h3 className="p-4 text-center text-white">{template.name}</h3>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div>
-        <div className="pl-8 pr-8">
-         <h1 className="text-xl font-bold mb-4">Task Board Templates</h1>
-         <p className="font-bold">Start your project with a Pump.task template to start pumping through tasks faster:</p>
-        </div>
-        <div className="grid grid-cols-3 gap-4 p-8 auto-rows-min">
-          {templates.map((template) => (
-            <div
-              key={template.id}
-              className="overflow-hidden rounded-lg bg-gray-700 min-h-32 font-bold">
-              <h3 className="p-4 text-white text-center">{template.name}</h3>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
     </>
   );
 }
