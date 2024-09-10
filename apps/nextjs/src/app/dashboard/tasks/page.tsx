4a8b04c892ac9b339c9b280d8c36f77400d61db3
samowen1992@hotmail.com <boss@DESKTOP-TJ1EF8N.>
2024-09-10 16:15:00 +1000

    feat: created task-card component export

---
+++ apps/nextjs/src/app/dashboard/tasks/page.tsx
@@ -0,0 +1,10 @@
+import TaskCard from "~/app/_components/task-card";
+
+export default function TasksPage() {
+  return (
+    <div>
+      <h1>Tasks Page</h1>
+      <TaskCard />
+    </div>
+  );
+}
