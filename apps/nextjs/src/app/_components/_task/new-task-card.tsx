385e9d1c7d28cdcda3492ed302d466e3d9225f3e
samowen1992@hotmail.com <boss@DESKTOP-TJ1EF8N.>
2024-09-19 11:46:30 +1000

    fix: refactored newtaskcard component to remove unused logic

--- apps/nextjs/src/app/_components/_task/new-task-card.tsx
+++ apps/nextjs/src/app/_components/_task/new-task-card.tsx
@@ -1,50 +1,19 @@
 "use client";
 
+import type { z } from "zod";
 import React from "react";
 
+import type { taskCardSchema } from "./task-card-schema";
 import { api } from "~/trpc/react"; // Ensure you import your API hook
 import TaskCardDialog from "./task-card-dialog";
-
-interface TaskCardData {
-  title: string;
-  description: string;
-  dueDate: string;
-  status: "To Do" | "In Progress" | "In QA" | "Done" | "Approved";
-  assignee: string;
-  tags: {
-    defaultTags: string[];
-    userGeneratedTags: string[];
-  };
-  customFields: {
-    fieldName: string;
-    fieldValue: string;
-  }[];
-}
 
 const NewTaskCard: React.FC = () => {
   const addTaskMutation = api.task.addTask.useMutation(); // Initialize your mutation
 
-  const handleSubmit = async (taskData: TaskCardData) => {
+  const handleSubmit = async (taskData: z.infer<typeof taskCardSchema>) => {
     try {
-      const selectedDefaultTags = taskData.tags.defaultTags;
-      const selectedUserTags = taskData.tags.userGeneratedTags;
-
-      // Prepare the data to be sent to the server
-      const formattedTaskData = {
-        title: taskData.title,
-        description: taskData.description,
-        dueDate: taskData.dueDate,
-        status: taskData.status,
-        assignee: taskData.assignee,
-        tags: {
-          defaultTags: selectedDefaultTags,
-          userGeneratedTags: selectedUserTags,
-        },
-        customFields: taskData.customFields,
-      };
-
-      // Send the task data to the tRPC mutation
-      await addTaskMutation.mutateAsync(formattedTaskData);
+      // Send the task data (validated in TaskCardDialog) to the tRPC mutation
+      await addTaskMutation.mutateAsync(taskData);
 
       // Handle success
       console.log("Task created successfully");
