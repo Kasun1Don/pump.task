7857f1251c26f9dc1221bfb2fb61cb80ee20e3ab
samowen1992@hotmail.com <boss@DESKTOP-TJ1EF8N.>
2024-09-26 10:00:52 +1000

    fix: adjusted status & tasks schema's to include extra properties

--- packages/db/src/schema/StatusSchema.ts
+++ packages/db/src/schema/StatusSchema.ts
@@ -9,15 +9,24 @@ import {
 
 import { ProjectClass } from "./Projects";
 
-@modelOptions({ schemaOptions: { collection: "status" } })
+@modelOptions({
+  schemaOptions: {
+    collection: "status",
+    timestamps: true, // Enables automatic createdAt and updatedAt fields
+  },
+})
 export class StatusClass {
-  // Status Name
+  // Status Name (e.g., "To Do", "In Progress", "Done")
   @prop({ required: true })
-  public name!: string;
+  public statusName!: string;
 
-  // Reference to the Project ID
+  // Reference to the Project ID (Which project this status belongs to)
   @prop({ ref: () => ProjectClass, required: true })
   public project!: Ref<ProjectClass>;
+
+  // Order of the status column (used to arrange location of status columns)
+  @prop({ required: true })
+  public order!: number;
 }
 
 export const Status =
--- packages/db/src/schema/TasksSchema.ts
+++ packages/db/src/schema/TasksSchema.ts
@@ -11,6 +11,7 @@ import { Project, ProjectClass } from "./Projects";
 import { StatusClass } from "./StatusSchema";
 import { TagsSchema } from "./TagsSchema";
 
+// Custom field name & values (Used when user adds a custom field to the task form)
 class CustomField {
   @prop({ required: true })
   public fieldName!: string;
@@ -19,11 +20,14 @@ class CustomField {
   public fieldValue!: string;
 }
 
+@modelOptions({
+  schemaOptions: {
+    collection: "tasks",
+    timestamps: true, // Enables automatic createdAt and updatedAt fields
+  },
+})
 export class TasksSchema {
-  /* 
-    Tags - References the 'TagsSchema' while running an additional check
-    to make sure atleast 1 tag is selected (either default or custom tags).
-  */
+  // Tags for categorizing tasks (includes default and user-generated)
   @prop({
     required: true,
     _id: false,
@@ -40,39 +44,40 @@ export class TasksSchema {
   })
   public tags!: TagsSchema;
 
-  // Title
+  // Title of the task
   @prop({ required: true })
   public title!: string;
 
-  // Description
+  // Description of the task
   @prop({ required: true })
   public description!: string;
 
-  // Due Date
+  // Due Date for the task
   @prop({ required: true })
   public dueDate!: Date;
 
-  // Assignee
+  // Assignee (User assigned to the task)
   @prop({ required: true })
   public assignee!: string;
 
-  // Custom Fields
+  // Custom Fields for additional information
   @prop({ type: () => [CustomField], required: false })
   public customFields?: CustomField[];
 
-  // Reference to the Status ID
+  // Reference to the Status the task belongs to
   @prop({ ref: () => StatusClass, required: true })
   public status!: Ref<StatusClass>;
 
-  // Reference to the Project ID
+  // Reference to the Project the task belongs to
   @prop({ ref: () => Project, required: true })
   public project!: Ref<ProjectClass>;
-}
 
-@modelOptions({ schemaOptions: { collection: "tasks" } })
-export class TaskClass extends TasksSchema {}
+  // Order of the task within column (Used to arrange location of task within status column)
+  @prop({ required: true })
+  public order!: number;
+}
 
 export const Task =
   (mongoose.models.TaskClass as
-    | ReturnModelType<typeof TaskClass>
-    | undefined) ?? getModelForClass(TaskClass);
+    | ReturnModelType<typeof TasksSchema>
+    | undefined) ?? getModelForClass(TasksSchema);
