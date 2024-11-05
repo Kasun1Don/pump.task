7c8f456760013f9b36ad86b326120d62deefa21b
Kasun1Don <144102327+Kasun1Don@users.noreply.github.com>
2024-11-05 10:50:39 +1000

    fix: ui width issue

--- apps/nextjs/src/app/_components/_projects/projects-skeleton.tsx
+++ apps/nextjs/src/app/_components/_projects/projects-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@acme/ui/skeleton";
 const ProjectsSkeleton = () => {
   // 3 x 3 skeleton cards to resemble the project cards
   return (
-    <div className="grid auto-rows-min grid-cols-3 gap-4 pt-8">
+    <div className="mx-auto grid max-w-7xl auto-rows-min grid-cols-3 gap-4 pt-8">
       {/* create an array with 9 empty slots and maps a skeleton card for each slot*/}
       {Array.from({ length: 9 }).map((_, i) => (
         <Skeleton
