5ffff9ac2604d4d3d30ef206328c366ba4ee7c6e
MikeLS95 <michaellaishleys@gmail.com>
2024-11-05 16:42:20 +1000

    style: adjusted New Task so on button click dialog is reset to default

--- apps/nextjs/src/app/_components/_task/task-card-dialog.tsx
+++ apps/nextjs/src/app/_components/_task/task-card-dialog.tsx
@@ -116,6 +116,7 @@ const TaskCardDialog = ({
     setValue,
     watch,
     trigger,
+    reset,
     formState: { errors },
   } = useForm<NewTaskCard | TaskCard>({
     resolver: zodResolver(isNewTask ? NewTaskCardSchema : TaskCardSchema),
@@ -581,6 +582,21 @@ const TaskCardDialog = ({
               if (isNewTask) {
                 const newTaskData: NewTaskCard = taskData;
                 onSubmit(newTaskData);
+
+                reset({
+                  title: "",
+                  description: "",
+                  dueDate: new Date(NaN),
+                  statusId: statusId,
+                  assigneeId: "unassigned",
+                  projectId: projectId,
+                  order: 0,
+                  tags: {
+                    defaultTags: [],
+                    userGeneratedTags: [],
+                  },
+                  customFields: [],
+                });
 
                 setIsDialogOpen(false);
               } else {
